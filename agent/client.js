0bb9ad15659aeba5cd8c5697e751c84c6fbf6f77
Stefan Beyer <info@wapplications.net>
2018-09-29 11:50:16 +0200

    date fix

--- agent/client.js
+++ agent/client.js
@@ -17,10 +17,10 @@ let allCalenders =
 
 {
   maria: [
-    {title: 'Night', date: '2018-09-29', start: '0:00',  end: '06:00',   level:0 },
-    {title: 'breakfest', date: '2018-09-29', start: '6:00',  end: '7:00',   level:0 },
-    {title: 'driving to shool and work',   date: '2018-09-29', start: '7:00',  end: '8:00',   level:0 },
-    {title: 'meeting', date: '2018-09-29',start: '9:00',  end: '11:00',  level:0 },
+    {title: 'Night', date: '2018-09-29', start: '00:00',  end: '06:00',   level:0 },
+    {title: 'breakfest', date: '2018-09-29', start: '06:00',  end: '07:00',   level:0 },
+    {title: 'driving to shool and work',   date: '2018-09-29', start: '07:00',  end: '08:00',   level:0 },
+    {title: 'meeting', date: '2018-09-29',start: '09:00',  end: '11:00',  level:0 },
     {title: 'business lunch', date: '2018-09-29', start: '12:00', end: '14:00',  level:0 },
     {title: 'work on project', date: '2018-09-29', start: '14:00', end: '16:00',  level:0 },
     {title: 'work on project', date: '2018-09-29', start: '18:00', end: '20:00',  level:0 },
@@ -28,25 +28,25 @@ let allCalenders =
     {title: 'Night', date: '2018-09-29', start: '22:00', end: '23:00',  level:0 }
   ],
   lukas: [
-    {title: 'Night', date: '2018-09-29', start: '0:00',  end: '06:00',   level:0 },
-    {title: 'breakfast', date: '2018-09-29', start: '6:00',  end: '7:00',  level:0 },
-    {title: 'driving to shool', date: '2018-09-29', start: '7:00',  end: '8:00',  level:0 },
-    {title: 'shool', date: '2018-09-29', start: '8:00', end: '13:00',  level:0 },
+    {title: 'Night', date: '2018-09-29', start: '00:00',  end: '06:00',   level:0 },
+    {title: 'breakfast', date: '2018-09-29', start: '06:00',  end: '07:00',  level:0 },
+    {title: 'driving to shool', date: '2018-09-29', start: '07:00',  end: '08:00',  level:0 },
+    {title: 'shool', date: '2018-09-29', start: '08:00', end: '13:00',  level:0 },
     {title: 'grandma', date: '2018-09-29', start: '13:00', end: '16:00',  level:0 },
     {title: 'Dinner with family', date: '2018-09-29', start: '20:00', end: '22:00',  level:0 }
   ],
   kim: [
-    {title: 'Night', date: '2018-09-29', start: '0:00',  end: '06:00',   level:0 },
-    {title: 'breakfast', date: '2018-09-29', start: '6:00',  end: '7:00',  level:0 },
-    {title: 'driving to shool', date: '2018-09-29', start: '7:00',  end: '8:00',  level:0 },
-    {title: 'shool', date: '2018-09-29', start: '8:00', end: '16:00',  level:0 },
+    {title: 'Night', date: '2018-09-29', start: '00:00',  end: '06:00',   level:0 },
+    {title: 'breakfast', date: '2018-09-29', start: '06:00',  end: '07:00',  level:0 },
+    {title: 'driving to shool', date: '2018-09-29', start: '07:00',  end: '08:00',  level:0 },
+    {title: 'shool', date: '2018-09-29', start: '08:00', end: '16:00',  level:0 },
     {title: 'Dinner with family', date: '2018-09-29', start: '20:00', end: '22:00',  level:0 }
   ],
   tom: [
-    {title: 'Night', date: '2018-09-29', start: '0:00',  end: '06:00',   level:0 },
+    {title: 'Night', date: '2018-09-29', start: '00:00',  end: '06:00',   level:0 },
     {title: 'breakfast', date: '2018-09-29', start: '06:00',  end: '09:30',  level:0 },
-    {title: 'driving to work', date: '2018-09-29', start: '7:00',  end: '8:00',  level:0 },
-    {title: 'customer meeting', date: '2018-09-29', start: '8:00', end: '12:00',  level:0 },
+    {title: 'driving to work', date: '2018-09-29', start: '07:00',  end: '08:00',  level:0 },
+    {title: 'customer meeting', date: '2018-09-29', start: '08:00', end: '12:00',  level:0 },
     {title: 'lunch', date: '2018-09-29', start: '12:00', end: '13:00',  level:0 },
     {title: 'office meeting', date: '2018-09-29', start: '13:00', end: '15:00',  level:0 },
     {title: 'work on project', date: '2018-09-29', start: '19:10', end: '20:00',  level:0 },
